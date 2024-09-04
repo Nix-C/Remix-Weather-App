0e55f3bc0e37d633a20724b11fece33a2a85166f
Nathaniel Campbell <nixc.web@gmail.com>
2024-09-04 11:23:55 -0500

    Adding "powered by" per Tomorrow.io docs

--- app/root.tsx
+++ app/root.tsx
@@ -19,6 +19,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <footer>
+          <small>
+            Powered by{" "}
+            <a href="https://www.tomorrow.io/" rel="noreferrer" target="_blank">
+              Tomorrow.io
+            </a>
+          </small>
+        </footer>
         <ScrollRestoration />
         <Scripts />
       </body>
